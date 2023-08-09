c2a21dfdc1b159cd69d45d55a3c1223ed4a06218
Azriel <azrihell.l@gmail.com>
2023-08-09 18:23:47 -0500

    added env support for custom host and port

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
+import pjson from "./package.json" assert { type: "json" };
 import cors from 'cors'
 import connectDB from './mongodb/connect.js'
 import postRoutes from './routes/postRoutes.js'
 import dalleRoutes from './routes/dalleRoutes.js'
-
-dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 app.use('/api/v1/post', postRoutes)
 app.use('/api/v1/dalle', dalleRoutes)
+app.env = dotenv.config().parsed
 
 app.get('/', async (req, res) => {
-  res.send('Hello from DALL-E')
+  res.json({
+    version: `v${pjson?.version}` || "v1.0.0",
+    message: 'Hello from DALL-E'
+  })
 })
 
-const startServer = async () => {
+app.listen(app.env.PORT, app.env.HOSTNAME, async () => {
   try {
     connectDB(process.env.MONGODB_URL)
-    app.listen(8080, () => console.log('Server started on port 8080'))
+    console.log(`Server running at http://${app.env.HOSTNAME}:${app.env.PORT}/`);
   } catch (error) {
     console.log(error)
   }
-};
-
-startServer()
+})
