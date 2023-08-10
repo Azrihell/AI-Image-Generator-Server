0ff198d72a4e4e96df229da61cc1655f26fd4aec
Azriel <azrihell.l@gmail.com>
2023-08-09 19:15:28 -0500

    restore

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-const cors = require('cors')
+// index.js
 const express = require('express')
-const pjson = require("./package.json")
-// const connectDB = require( './mongodb/connect.js')
-// const postRoutes = require( './routes/postRoutes.js')
-// const dalleRoutes = require( './routes/dalleRoutes.js')
 
-const server = express()
-server.use(express.json({ limit: '50mb' }))
-server.use(express.urlencoded({ extended: true }))
-server.env = require('dotenv').config().parsed
+const app = express()
+const PORT = 4000
 
-
-server.use(cors())
-// server.use('/api/v1/post', postRoutes)
-// server.use('/api/v1/dalle', dalleRoutes)
-
-
-server.get('/', async (req, res) => {
-  res.json({
-    version: pjson.version,
-    message: 'Hello from DALL-E'
-  })
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
 })
 
-server.listen({ port: server.env.PORT }, async () => {
-  try {
-    // connectDB(process.env.MONGODB_URL)
-    console.log(`Server running at http://localhost:${server.env.PORT}`);
-  } catch (error) {
-    console.log(error)
-  }
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
 })
 
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
 
 // Export the Express API
-module.exports = server
+module.exports = app
+
+
+// const cors = require('cors')
+// const express = require('express')
+// const pjson = require("./package.json")
+// // const connectDB = require( './mongodb/connect.js')
+// // const postRoutes = require( './routes/postRoutes.js')
+// // const dalleRoutes = require( './routes/dalleRoutes.js')
+
+// const server = express()
+// server.use(express.json({ limit: '50mb' }))
+// server.use(express.urlencoded({ extended: true }))
+// server.env = require('dotenv').config().parsed
+
+
+// server.use(cors())
+// // server.use('/api/v1/post', postRoutes)
+// // server.use('/api/v1/dalle', dalleRoutes)
+
+
+// server.get('/', async (req, res) => {
+//   res.json({
+//     version: pjson.version,
+//     message: 'Hello from DALL-E'
+//   })
+// })
+
+// server.listen({ port: server.env.PORT }, async () => {
+//   try {
+//     // connectDB(process.env.MONGODB_URL)
+//     console.log(`Server running at http://localhost:${server.env.PORT}`);
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
+
+
+// // Export the Express API
+// module.exports = server
