f4df22ebc6b353d0e0342ba75af4ed64b28f667a
Azriel <azrihell.l@gmail.com>
2023-08-09 21:26:01 -0500

    removed connection files

--- index.ts
+++ index.ts
@@ -25,8 +25,8 @@ server.get('/', (_req: Request, res: Response) => {
 
 
 // AZ: Import sub routes
-DalleRoutes(server)
-PostsRoutes(server)
+// DalleRoutes(server)
+// PostsRoutes(server)
 
 
 // AZ: Start the express server. 
