5932db42e706313808bff73b371a9b58c61c10e4
Azriel <azrihell.l@gmail.com>
2023-08-09 19:36:09 -0500

    Add Support Services

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-// index.js
+// Vercel requires cjs (commonJs) so we're going to have to use require() and module.exports in this project to create `function` server
 const express = require('express')
 const pjson = require("./package.json")
+
+
+// // const connectDB = require( './mongodb/connect.js')
+// // const postRoutes = require( './routes/postRoutes.js')
+// // const dalleRoutes = require( './routes/dalleRoutes.js')
+
+
+// 
+// // server.use('/api/v1/post', postRoutes)
+// // server.use('/api/v1/dalle', dalleRoutes)
+
+
+// Create the Express Server
 const server = express()
+server.use(express.json({ limit: '50mb' }))
+server.use(express.urlencoded({ extended: true }))
 server.env = require('dotenv').config().parsed
+server.use(cors())
 
 
+// Create a default API route to return some useful server information
 server.get('/', async (req, res) => {
   res.json({
     version: pjson.version,
-    message: 'Hello from DALL-E'
+    message: 'Hello form Az! This is the API service for my DALL-E like application. Enjoy 🥳'
   })
 })
 
 
-server.get('/test', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
-
-server.get('/logs', (req, res) => {
-  res.send(JSON.stringify(process.env))
-})
-
-server.get('/port', (req, res) => {
-  res.send(JSON.stringify(process.env.PORT))
-})
-
-
+// Start the express server. 
 server.listen(process.env.PORT, async () => {
   try {
     // connectDB(process.env.MONGODB_URL)
@@ -40,27 +45,3 @@ server.listen(process.env.PORT, async () => {
 module.exports = server
 
 
-// const cors = require('cors')
-// const express = require('express')
-// const pjson = require("./package.json")
-// // const connectDB = require( './mongodb/connect.js')
-// // const postRoutes = require( './routes/postRoutes.js')
-// // const dalleRoutes = require( './routes/dalleRoutes.js')
-
-// const server = express()
-// server.use(express.json({ limit: '50mb' }))
-// server.use(express.urlencoded({ extended: true }))
-// server.env = require('dotenv').config().parsed
-
-
-// server.use(cors())
-// // server.use('/api/v1/post', postRoutes)
-// // server.use('/api/v1/dalle', dalleRoutes)
-
-
-// server.get('/', async (req, res) => {
-//   res.json({
-//     version: pjson.version,
-//     message: 'Hello from DALL-E'
-//   })
-// })
